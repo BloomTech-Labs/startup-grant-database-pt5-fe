4d968aaca809a6388f5ef861be54f7f18dce0c39
Ernesto Pena <ernestopenad@gmail.com>
2019-11-08 21:42:07 -0600

    Update Login.js
    Directly from Fritz

--- client/src/Authentication/Login.js
+++ client/src/Authentication/Login.js
@@ -1,100 +1,64 @@
-import React, { useEffect, useState } from "react";
-// import useGlobal from '../store';
-
-import { firebase } from "../helpers/index";
-
-const firebaseUser = require("firebase");
-
-const Login = () => {
-  //to use the  action for passing the user data to the api
-  //   const [globalState, globalActions] = useGlobal();
-
-  useEffect(() => {
-    firebase();
-    //Manage Users below after sign in/sign up but it was moved to the
-    //login component to send the request to api using hooks
-    firebaseUser.auth().onAuthStateChanged(function(user) {
-      if (user) {
-        // User is signed in.
-        //Get token to pass to headers for PrivateRoute to authenticate user
-        console.log("You are currently logged in!");
-        user.getIdToken().then(token => {
-          //   console.log(token);
-          localStorage.setItem("authorization", token);
-          //passing the user info to server
-          // globalActions.Login(user);
-        });
-      } else {
-        // No user is signed in.
-        console.log("You are currently logged out");
-        localStorage.removeItem("authorization");
-
-import { firebase } from '../helpers/index';
+import React, { useEffect, useState } from ‘react’;
+// import useGlobal from ‘../store’;
+import { firebase } from ‘../helpers/index’;
 //importing axios for http request to api
-import axios from 'axios';
-
+import axios from ‘axios’;
 //Requiring only the packages
-const firebaseUser = require('firebase/app');
-require('firebase/auth');
-
+const firebaseUser = require(‘firebase/app’);
+require(‘firebase/auth’);
 const Login = props => {
-  const [requestError, setRequestError] = useState('');
-
-  useEffect(() => {
-    firebase();
-    //HTTP request to API
-    firebaseUser.auth().onAuthStateChanged(function(currentUser) {
-      if (currentUser) {
-        currentUser
-          .getIdToken(/* forceRefresh */ false)
-          .then(function(idToken) {
-            // console.log('Token: ', idToken);
-            const token = { idToken: idToken };
-            axios
-              .post(`${process.env.REACT_APP_API}/api/users/login`, token)
-              .then(res => {
-                //Succesful login
-
-                //SAVE USER ID TO LOCAL STORAGE
-                const id = res.data.id;
-                // console.log('User ID', id);
-                localStorage.setItem('id', id);
-
-                // SAVE TOKEN TO LOCAL STORAGE FOR PRIVATE ROUTE
-                localStorage.setItem('authorization', idToken);
-                props.history.push('/dashboard');
-              })
-              .catch(err => {
-                //Invalid token
-                console.log('Invalid Token or DB currently down', err);
-                setRequestError(err.response);
-              });
-          })
-          .catch(function(error) {
-            // Handle error
-            console.log('Error occur during last catch http request', error);
-            setRequestError(error.response.status);
-          });
-      } else {
-        //User is currently logged out.
-        console.log('You are currently logged out');
-        //Remove token, firebase and id from local storage after sign out
-        localStorage.removeItem('authorization');
-        localStorage.removeItem('firebaseui::rememberedAccounts');
-        localStorage.removeItem('id');
-      }
-    });
-  }, [props]);
-
-  return (
-    <div>
-      {requestError === 400 && (
-        <h4 className="alert">Unauthorized, try again later.</h4>
-      )}
-      <div id="firebaseui-auth-container"></div>
-      <div id="loader"></div>
-    </div>
-  );
+ const [requestError, setRequestError] = useState(‘’);
+ useEffect(() => {
+   firebase();
+   //HTTP request to API
+   firebaseUser.auth().onAuthStateChanged(function(currentUser) {
+     if (currentUser) {
+       currentUser
+         .getIdToken(/* forceRefresh */ false)
+         .then(function(idToken) {
+           // console.log(‘Token: ’, idToken);
+           const token = { idToken: idToken };
+           axios
+             .post(${process.env.REACT_APP_API}/api/users/login, token)
+             .then(res => {
+               //Succesful login
+               //SAVE USER ID TO LOCAL STORAGE
+               const id = res.data.id;
+               // console.log(‘User ID’, id);
+               localStorage.setItem(‘id’, id);
+               // SAVE TOKEN TO LOCAL STORAGE FOR PRIVATE ROUTE
+               localStorage.setItem(‘authorization’, idToken);
+               props.history.push(‘/dashboard’);
+             })
+             .catch(err => {
+               //Invalid token
+               console.log(‘Invalid Token or DB currently down’, err);
+               setRequestError(err.response);
+             });
+         })
+         .catch(function(error) {
+           // Handle error
+           console.log(‘Error occur during last catch http request’, error);
+           setRequestError(error.response.status);
+         });
+     } else {
+       //User is currently logged out.
+       console.log(‘You are currently logged out’);
+       //Remove token, firebase and id from local storage after sign out
+       localStorage.removeItem(‘authorization’);
+       localStorage.removeItem(‘firebaseui::rememberedAccounts’);
+       localStorage.removeItem(‘id’);
+     }
+   });
+ }, [props]);
+ return (
+   <div>
+     {requestError === 400 && (
+       <h4 className=“alert”>Unauthorized, try again later.</h4>
+     )}
+     <div id=“firebaseui-auth-container”></div>
+     <div id=“loader”></div>
+   </div>
+ );
 };
-
 export default Login;
