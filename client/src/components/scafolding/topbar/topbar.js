42dbcee058fb229ff608e502fda653d4638afccf
fritzgt <tuanis@live.com>
2019-11-14 13:16:15 +0200

    Finalized fixes

--- client/src/components/scafolding/topbar/topbar.js
+++ client/src/components/scafolding/topbar/topbar.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { NavLink, Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import './Grantify.png';
 import './topbar.css';
