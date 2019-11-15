c6007b6b7fb08ddff15445bfcf3f19667494c308
Jonathan Traverso <jtraverso7@gmail.com>
2019-11-15 00:03:26 -0500

    Initial card component and styles functional.

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,14 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Card, CardContent } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles(() => ({
+  cardContainer: {
+    display: "flex",
+    flexFlow: "row wrap"
+  },
+  card: {
+    width: "32%",
+    margin: "0 auto"
+  },
+  cardDetails: {
+    border: "1px solid dark blue"
+  },
+  linkStyling: {
+    textDecoration: "none"
+  }
+}));
 
 const LandingPage = () => {
+  const classes = useStyles();
   return (
-    <Card>
-      <CardContent>
-        <Link to="/search">Get Granted</Link>
-      </CardContent>
-    </Card>
+    <div classname={classes.cardContainer}>
+      <Card className={classes.card}>
+        <CardContent classname={classes.cardDetails}>
+          <Link to="/search" className={classes.linkStyling}>
+            Get Granted
+          </Link>
+        </CardContent>
+      </Card>
+    </div>
   );
 };
 
