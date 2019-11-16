a153e2b510844372a1034ff7a324704e1de29815
Jonathan Traverso <jtraverso7@gmail.com>
2019-11-16 14:30:41 -0500

    Cards scaffolded with minimal styling.

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -6,11 +6,12 @@ import { makeStyles } from "@material-ui/styles";
 const useStyles = makeStyles(() => ({
   cardContainer: {
     display: "flex",
-    flexFlow: "row wrap"
+    flexFlow: "row no wrap"
   },
   card: {
     width: "32%",
-    margin: "0 auto"
+    margin: "0 0 3% 0"
+    // marginBottom: "3%"
   },
   cardDetails: {
     border: "1px solid dark blue"
@@ -23,14 +24,60 @@ const useStyles = makeStyles(() => ({
 const LandingPage = () => {
   const classes = useStyles();
   return (
-    <div classname={classes.cardContainer}>
-      <Card className={classes.card}>
-        <CardContent classname={classes.cardDetails}>
-          <Link to="/search" className={classes.linkStyling}>
-            Get Granted
-          </Link>
-        </CardContent>
-      </Card>
+    <div>
+      {/* added an extra div below to test spacing */}
+      <div classname={classes.cardContainer}>
+        <Card className={classes.card}>
+          <CardContent classname={classes.cardDetails}>
+            <h3>Welcome to Grantlify!</h3>
+          </CardContent>
+        </Card>
+        {/*  */}
+        <Card className={classes.card}>
+          <CardContent classname={classes.cardDetails}>
+            <h3>Our Goal</h3>
+            <p>
+              Hundreds and thousands of dollars worth of unused grant money is
+              thrown away annually. Our Goal is to take all of the grants that
+              are somehow overlooked and put them in one central location... for
+              you.
+            </p>
+          </CardContent>
+        </Card>
+        {/*  */}
+        <Card className={classes.card}>
+          <CardContent classname={classes.cardDetails}>
+            <h3>How It Works</h3>
+            <p></p>
+          </CardContent>
+        </Card>
+        {/*  */}
+        <Card className={classes.card}>
+          <CardContent classname={classes.cardDetails}>
+            <h3>Finding Your Grants</h3>
+            <p></p>
+          </CardContent>
+        </Card>
+        {/*  */}
+        <Card className={classes.card}>
+          <CardContent classname={classes.cardDetails}>
+            <h3>What's the Catch?</h3>
+            <p></p>
+          </CardContent>
+        </Card>
+        {/*  */}
+        <Card className={classes.card}>
+          <CardContent classname={classes.cardDetails}>
+            <p>If you like what you see...</p>
+            <Link to="/search" className={classes.linkStyling}>
+              Get Granted
+            </Link>
+          </CardContent>
+        </Card>
+        {/*  */}
+
+        {/*  */}
+      </div>
     </div>
   );
 };
