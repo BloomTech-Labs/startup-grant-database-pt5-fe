eacd013403f17702425e4b2bd862f366a90bae12
Claire Sinozich <claire.sinozich@gmail.com>
2019-12-04 13:04:10 -0500

    dashboard hooked into back end - not sure if working...

--- client/src/components/dashboard/New.js
+++ client/src/components/dashboard/New.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/styles";
 import { Container, Card } from "@material-ui/core";
+
+import axios from "axios";
 
 import NewCard from "./NewCard";
 
@@ -10,27 +12,41 @@ const useStyles = makeStyles(() => ({}));
 const New = props => {
   const { className, ...rest } = props;
 
+  const [newGrants, setNewGrants] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://startup-grant-database.herokuapp.com/api/users/1")
+      .then(res => {
+        console.log(res);
+        setNewGrants(res.savedGrants.data);
+      })
+      .catch(err => {
+        console.error(err.message);
+      });
+  });
+
   const classes = useStyles();
 
-  const grant = [
-    {
-      id: 1,
-      name: "This is the name of a grant",
-      duedate: "10/23/17",
-      amount: "5000"
-    },
-    {
-      id: 2,
-      name: "this is grant #2",
-      duedate: "10/11/11",
-      amount: "6345"
-    }
-  ];
+  // const grant = [
+  //   {
+  //     id: 1,
+  //     name: "This is the name of a grant",
+  //     duedate: "10/23/17",
+  //     amount: "5000"
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "this is grant #2",
+  //     duedate: "10/11/11",
+  //     amount: "6345"
+  //   }
+  // ];
 
   return (
     <Container style={{ backgroundColor: "#F0FDFE", height: "100%" }}>
       <h2>New Grants for You</h2>
-      {grant.map(items => {
+      {newGrants.map(items => {
         return (
           <Link
             style={{ textDecoration: "none", color: "#000000" }}
--- client/src/components/dashboard/Saved.js
+++ client/src/components/dashboard/Saved.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/styles";
 import { Container } from "@material-ui/core";
+
+import axios from "axios";
 
 import SavedCard from "./SavedCard";
 
@@ -15,27 +17,41 @@ const useStyles = makeStyles(theme => ({
 const Saved = props => {
   const { className } = props;
 
+  const [savedGrants, setSavedGrants] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://startup-grant-database.herokuapp.com/api/users/1")
+      .then(res => {
+        console.log(res);
+        setSavedGrants(res.savedGrants.data);
+      })
+      .catch(err => {
+        console.error(err.message);
+      });
+  });
+
   const classes = useStyles();
 
-  const grant = [
-    {
-      id: 1,
-      name: "This is the name of a grant",
-      duedate: "10/23/17",
-      amount: "5000"
-    },
-    {
-      id: 2,
-      name: "this is grant #2",
-      duedate: "10/11/11",
-      amount: "6345"
-    }
-  ];
+  // const grant = [
+  //   {
+  //     id: 1,
+  //     name: "This is the name of a grant",
+  //     duedate: "10/23/17",
+  //     amount: "5000"
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "this is grant #2",
+  //     duedate: "10/11/11",
+  //     amount: "6345"
+  //   }
+  // ];
 
   return (
     <Container>
       <h2>Saved Grants</h2>
-      {grant.map(items => {
+      {savedGrants.map(items => {
         return (
           <Link className={classes.link} to={`/search/${items.id}`}>
             <SavedCard key={items.id} grant={items} />
