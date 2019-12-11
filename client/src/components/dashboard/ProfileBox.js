e8e3f9cfa2d7fe40bfe76a4ab0573766b2745dc9
tjforeman <tylerforeman92@gmail.com>
2019-12-10 22:38:43 -0500

    typo

--- client/src/components/dashboard/ProfileBox.js
+++ client/src/components/dashboard/ProfileBox.js
@@ -56,7 +56,7 @@ const ProfileBox = props => {
   //
   // <Avatar src={values[0].avatar} className={classes.image} />
 
-  if (0 > values.length {
+  if (0 > values.length) {
     return <h1>Loading...</h1>;
   } else {
     return (
