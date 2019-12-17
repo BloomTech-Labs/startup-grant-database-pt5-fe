2324e3dcf7e6a9f53618d9fd87966a77d6721130
fritzgt <tuanis@live.com>
2019-12-17 04:45:25 +0200

    connected dashboard saved grants to back end

--- client/src/components/dashboard/Saved.js
+++ client/src/components/dashboard/Saved.js
@@ -1,76 +1,43 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { makeStyles } from "@material-ui/styles";
-import { Container } from "@material-ui/core";
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/styles';
+import { Container } from '@material-ui/core';
 
-import axios from "axios";
+import axios from 'axios';
 
-import SavedCard from "./SavedCard";
+import SavedCard from './SavedCard';
 
 const useStyles = makeStyles(theme => ({
   link: {
-    textDecoration: "none",
-    color: "#000000"
+    textDecoration: 'none',
+    color: '#000000'
   }
 }));
 
 const Saved = props => {
-  const { className } = props;
-
-  const id = localStorage.getItem("id");
+  const id = localStorage.getItem('id');
 
   const [savedGrants, setSavedGrants] = useState([]);
 
   useEffect(() => {
-    axios
-      .get(
-        "https://startup-grant-database-staging.herokuapp.com/api/users/${id}"
-      )
-      .then(res => {
-        console.log(res);
-        setSavedGrants(res.data.pinnedGrants);
-      })
-      .catch(err => {
-        console.error(err.message);
-      });
-
-    //alternate code
-    // const fetchAll = async () => {
-    //   //Fetch
-    //   const userResult = await axios(
-    //     "https://startup-grant-database-staging.herokuapp.com/api/users/1"
-    //   );
-    //   setSavedGrants(userResult.data.pinnedGrants);
-    // };
-    // fetchAll();
+    const fetchAll = async () => {
+      const savedGrants = await axios(
+        `${process.env.REACT_APP_API}/api/savedgrants/${id}`
+      );
+      setSavedGrants(savedGrants.data);
+    };
+    fetchAll();
   }, []);
-
-  console.log("savedgrants", savedGrants);
 
   const classes = useStyles();
 
-  // const grant = [
-  //   {
-  //     id: 1,
-  //     name: "This is the name of a grant",
-  //     duedate: "10/23/17",
-  //     amount: "5000"
-  //   },
-  //   {
-  //     id: 2,
-  //     name: "this is grant #2",
-  //     duedate: "10/11/11",
-  //     amount: "6345"
-  //   }
-  // ];
-
-  if (savedGrants == undefined) {
+  if (savedGrants === undefined) {
     return <h1>Loading...</h1>;
   } else {
     return (
       <Container>
         <h2>Saved Grants</h2>
-        {savedGrants.slice(0, 5).map(items => {
+        {savedGrants.map(items => {
           return (
             <Link className={classes.link} to={`/search/${items.id}`}>
               <SavedCard key={items.id} grant={items} />
