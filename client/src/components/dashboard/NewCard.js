14fe819f9a61f6bab9cc92eafa057f3fab44f654
Scott Schindler <scottschindler29@gmail.com>
2020-01-07 21:04:18 -0500

    old changes

--- client/src/components/dashboard/NewCard.js
+++ client/src/components/dashboard/NewCard.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { Card } from '@material-ui/core';
-import Moment from 'react-moment';
+import React from "react";
+import { Card } from "@material-ui/core";
+//import Moment from 'react-moment';
 
 const NewCard = props => {
   return (
     <Card>
       <h3>{props.grant.grant_title}</h3>
-      <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-        <div style={{ display: 'flex' }}>
+      <div style={{ display: "flex", justifyContent: "space-around" }}>
+        <div style={{ display: "flex" }}>
           <p>
-            Due : <Moment format="YYYY/MM/DD">{props.grant.due_date}</Moment>
+            {/* Due : <Moment format="YYYY/MM/DD">{props.grant.due_date}</Moment> */}
           </p>
         </div>
-        <div style={{ display: 'flex' }}>
+        <div style={{ display: "flex" }}>
           <p>Amount: {props.grant.grant_amount}</p>
         </div>
       </div>
