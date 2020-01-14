008f689b95cff340d34506ea82622ddd3c214aa8
tjforeman <tylerforeman92@gmail.com>
2020-01-13 19:35:12 -0500

    working

--- client/src/components/grant-forms/GrantStatesFrom.js
+++ client/src/components/grant-forms/GrantStatesFrom.js
@@ -1,9 +1,19 @@
 import React,{useState, useEffect} from 'react'
 import axios from 'axios'
-
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
+import { makeStyles } from "@material-ui/core/styles";
+import FormLabel from '@material-ui/core/FormLabel';
+import Button from "@material-ui/core/Button";
 
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1)
+    }
+  }
+}));
 
 const GrantStatesForm = props => {
     const grantId = localStorage.getItem('grant_id')
@@ -14,6 +24,7 @@ const GrantStatesForm = props => {
 
        
       });
+      const classes = useStyles();
     
     useEffect(() => {
         const fetchAll = async () => {
@@ -52,11 +63,14 @@ const GrantStatesForm = props => {
 
       
 return(
+    
     <div>
+    <FormLabel>Please select the state your grant is for </FormLabel>
         <Select
     value={form.state_id}
     onChange={handleChange('state_id')}
     name="category"
+    placeholder="Select the state your grant is for "
     inputProps={{
       id: 'categories-required'
     }}
@@ -64,13 +78,18 @@ return(
     {states &&
       states.map(({id, state_name}) => {
         return (
-          <MenuItem key={id} value={id}>
+          <MenuItem key={id} value={id} >
             {state_name}
           </MenuItem>
         );
       })}
   </Select>
-  <button onClick = {onsubmit}></button>
+  
+  <div className={classes.root} >
+        <Button onClick = {onsubmit} variant="contained" color="primary">
+          Submit
+        </Button>
+    </div>
     
     </div>
 )
